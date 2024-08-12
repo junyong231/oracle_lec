aaed5ce9e8d995767115d34a979e6563b31dc4d7
junyong231 <bagjunyong14@gmail.com>
2024-08-13 01:25:19 +0900

    못푼문제 품

--- days06/SCOTT_6일차_20240812.sql
+++ days06/SCOTT_6일차_20240812.sql
@@ -604,3 +604,170 @@ FROM insa; --11개의 도시가 있다
 -- 오라클 10g부터 추가된 기능 : PARTITION BY OUT JOIN ..?
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--문제) emp 에서 ename,pay, 평균급여,  절상 , 절삭 (소수점 3자리)
+
+SELECT ename, sal+NVL(comm,0)
+        , (SELECT ROUND(AVG(sal+NVL(comm,0))) FROM emp)
+        , (SELECT  CEIL( AVG(sal+NVL(comm,0))* 100  )/100 FROM emp)
+FROM emp;
+
+-- 문제2) pay avg_pay, 많다 적다 같다 출력
+SELECT e.*, CASE WHEN e.pay > e.avg_pay THEN '많다'
+                 WHEN e.pay < e.avg_pay THEN '적다' END 평균비교
+FROM(
+SELECT ename, sal+NVL(comm,0) pay
+        ,(SELECT ROUND(AVG(sal+NVL(comm,0))) FROM emp) avg_pay
+FROM emp
+)e;
+
+--[문제] insa 테이블에서 오늘을 기준으로 생일이 지남 여부를 출력하는 쿼리를 작성하세요 . 
+--      ( '지났다', '안지났다', '오늘 ' 처리 )   
+-- 1) 1002 이순신 주민번호 오늘날짜의 월/일로 수정 (update)
+UPDATE insa
+SET ssn = SUBSTR(ssn,1,2) || '0812' || SUBSTR(ssn,7)
+WHERE name = '이순신' ;
+commit;
+SELECT *
+FROM insa;
+-- 2) 생일 지남 여부
+
+SELECT name, ssn, CASE WHEN SUBSTR(ssn,3,4) > TO_CHAR(sysdate,'mmdd') THEN '안지남'
+            WHEN SUBSTR(ssn,3,4) < TO_CHAR(sysdate,'mmdd') THEN '지남'
+            ELSE '오늘은 내 생일 'END 생일 
+FROM insa;
+
+--문제) insa에서 ssn을 가지고  만나이 계산해서 출력?
+-- 만나이 : 올해년도 - 생일년도  생일지났으면 그대로, 안지났으면 -1
+-- 성별자리가) 1,2 면 1900  3,4 2000  0,9 1800년대 ,, 56 외국인 1900 . 78 외국인 2000
+-- 문제) insa 테이블에서 남자 사원수, 여자 사원수
+
+
+
+SELECT  CASE WHEN SUBSTR(ssn,8,1) = 1 THEN '남자'
+              WHEN SUBSTR(ssn,8,1) = 2 THEN '여자'
+              END sex
+        ,COUNT ( SUBSTR(ssn,8,1) ) employee
+FROM insa
+GROUP BY SUBSTR(ssn,8,1);
+
+
+-- 이후 부서별 남자사원수ㅡ 여자사원수 출력
+
+SELECT buseo
+        ,CASE WHEN SUBSTR(ssn, 8, 1) =1 THEN '남자'
+              WHEN SUBSTR(ssn, 8, 1) =2 THEN '여자'
+              END 성별
+        ,COUNT(SUBSTR(ssn, 8, 1)) 사원수
+FROM insa
+GROUP BY buseo,SUBSTR(ssn, 8, 1)
+ORDER BY buseo;
+
+-- 문제) emp에서 최고 급여자, 최저 급여자  사원정보 출력 
+
+SELECT *
+FROM emp
+WHERE sal = (SELECT MAX(sal) FROM emp) OR sal = (SELECT MIN(sal) FROM emp) ;
+
+-- 문제) emp에서 부서별 최고 급여자, 최저 급여자  사원정보 출력 
+
+SELECT *
+FROM emp e
+WHERE sal = (SELECT MAX(sal) FROM emp WHERE deptno = e.deptno)
+;
+
+
+
+-- 문제  emp 테이블에서 comm이 400이하인 사원 조회 ( comm이 NULL이어도 400 이하)
+--문제) emp에서 sal이 상위 20퍼센트 해당되는 사원 정보?
+-- (순위 함수 사용)
+--문제) 다음 주 월요일은 휴강입니다 . 날짜 조회 ?
+--문제 emp에서 각 사원들의 입사일자를 기준으로 10년 5개월 20일째 되는 날짜를 출력하라 ?
+
+--insa 테이블에서 
+--[실행결과]
+--                                           부서사원수/전체사원수 == 부/전 비율
+--                                           부서의 해당성별사원수/전체사원수 == 부성/전%
+--                                           부서의 해당성별사원수/부서사원수 == 성/부%
+--                                           
+--부서명     총사원수 부서사원수 성별  성별사원수  부/전%   부성/전%   성/부%
+--개발부       60       14         F       8       23.3%     13.3%       57.1%
+--개발부       60       14         M       6       23.3%     10%       42.9%
+--기획부       60       7         F       3       11.7%       5%       42.9%
+--기획부       60       7         M       4       11.7%   6.7%       57.1%
+--영업부       60       16         F       8       26.7%   13.3%       50%
+--영업부       60       16         M       8       26.7%   13.3%       50%
+--인사부       60       4         M       4       6.7%   6.7%       100%
+--자재부       60       6         F       4       10%       6.7%       66.7%
+--자재부       60       6         M       2       10%       3.3%       33.3%
+--총무부       60       7         F       3       11.7%   5%           42.9%
+--총무부       60       7         M    4       11.7%   6.7%       57.1%
+--홍보부       60       6         F       3       10%       5%           50%
+--홍보부       60       6         M       
+
+SELECT e.*
+        , ROUND(e.부서별사원수/e.전체 , 3)* 100 || '%' "부/전 비율"
+        , ROUND(e.사원수/e.전체 , 3)* 100 || '%' "성/전 비율"
+        , ROUND(e.사원수/e.부서별사원수 , 3)* 100 || '%' "성/부 비율"
+FROM(
+    SELECT  buseo 부서명
+            , (SELECT COUNT(*) FROM insa) 전체
+            , (SELECT COUNT(buseo) FROM insa WHERE buseo=i.buseo) 부서별사원수
+            , DECODE(SUBSTR(SSN,8,1),1,'남자',2,'여자') 성별
+            , COUNT(*) 사원수
+    FROM insa i
+    GROUP BY buseo,SUBSTR(SSN,8,1)
+    ORDER BY buseo,성별 DESC
+)e;
+
+
+
+
+
+
+
+
+--문제 insa테이블에서 TOP_N분석방식으로 급여 많이 받는 탑텐 조회 ? 
+--문제 (emp테이블) (페이 백단위로 #한개 찍어서 그래프) (LPAD)
+--문제) emp테이블에서 
+--사원수가 가장많은부서,적은부서,사원수 출력
+
+--예2 emp에서 월별 입사한 사원수 조회 ?
+-- 1월   2월  3월 .....
+--   2      0    5  ....
+
+--문제) emp 테이블에서 job별 사원수 조회
+
+--문제) emp에서 부서별, 잡별 사원수 조회
+
+
+
+
